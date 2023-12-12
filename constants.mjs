2842f54276901c20e889cc297bbef01828258520
TripleAsterick <www.zheng813@gmail.com>
2023-12-11 20:04:50 -0600

    shared utils for both front and backend

---
+++ constants.mjs
@@ -0,0 +1,33 @@
+
+
+
+const PLAYER_ONE_STORAGE_KEY = "player one";
+const PLAYER_TWO_STORAGE_KEY = "player two";
+
+const NUM_BOARDS = 9;
+const NUM_TILES_PER_BOARD = 9;
+
+const CONTINUE = "continue";
+const DRAW = "draw";
+
+// timer consts
+const PLAYER_START_TIME = 10;
+
+// networking constants
+const INITIALIZE = "initialize";
+const NEW_MOVE = "new move";
+const WAIT_FOR_OTHER_PLAYER = "wait";
+const RESETTED = "resetted";
+const RESET = "reset";
+
+const GAME_OVER = "game over";
+
+const WINNER = "winner";
+const LOSER = "loser";
+
+
+export {
+    PLAYER_ONE_STORAGE_KEY, PLAYER_TWO_STORAGE_KEY, NUM_BOARDS, NUM_TILES_PER_BOARD, CONTINUE, DRAW,
+    PLAYER_START_TIME, INITIALIZE, NEW_MOVE, WINNER, LOSER, RESET, WAIT_FOR_OTHER_PLAYER, RESETTED,
+    GAME_OVER
+};
